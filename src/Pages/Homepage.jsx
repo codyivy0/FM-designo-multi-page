0b7b74d768781d82dcd84fac40d2bc8fc272fa41
codyivy0 <codyivy0@gmail.com>
2024-02-05 17:00:42 -0600

    banner mobile/todo desktop

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,9 +1,20 @@
+import Button from "../Components/Button/Button";
 import Footer from "../Components/Footer/Footer";
-
+import styles from "./Homepage.module.css";
 const Homepage = () => {
   return (
     <main>
-      HomePage
+      <section className={styles.banner}>
+        <div className={styles.bannerText}>
+          <h1>Award-winning custom designs and digital branding solutions</h1>
+          <p>
+            With over 10 years in the industry, we are experienced in creating
+            fully responsive websites, app design, and engaging brand experiences.
+            Find out more about our services.
+          </p>
+          <Button>LEARN MORE</Button>
+        </div>
+      </section>
       <Footer />
     </main>
   );
