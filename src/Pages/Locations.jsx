90c08b5790fbfe016b845c655bc3f68c744ed1c5
codyivy0 <codyivy0@gmail.com>
2024-02-09 14:41:37 -0600

    locations page responsive/grid areas ftw

--- src/Pages/Locations.jsx
+++ src/Pages/Locations.jsx
@@ -1,12 +1,75 @@
-import Footer from "../Components/Footer/Footer";
+import styles from "./Locations.module.css";
+import FooterWithCard from "../Components/FooterWithCard/FooterWithCard";
 
 const Locations = () => {
   return (
-    <div>
-      Locations
-      <Footer />
-    </div>
-  )
+    <>
+      <main>
+        <div className={styles.wrapper}>
+          <div className={`${styles.imageHolder} ${styles.canadaMap}`}>
+            <img
+              src="/images/locations/desktop/image-map-canada.png"
+              alt="canada location"
+            />
+          </div>
+          <div className={`${styles.locationText} ${styles.canadaText}`}>
+            <h1>Canada</h1>
+            <div>
+              <strong>Designo Central Office</strong>
+              <p>3886 Wellington Street</p>
+              <p>Toronto, Ontario M9C 3J5</p>
+            </div>
+            <div>
+              <strong>Contact</strong>
+              <p>P: +1 253-863-8967</p>
+              <p>M: contact@designo.co</p>
+            </div>
+          </div>
+
+          <div className={`${styles.imageHolder} ${styles.australiaMap}`}>
+            <img
+              src="/images/locations/desktop/image-map-australia.png"
+              alt="Australia location"
+            />
+          </div>
+          <div className={`${styles.locationText} ${styles.australiaText}`}>
+            <h1>Australia</h1>
+            <div>
+              <strong>Designo Central Office</strong>
+              <p>3886 Wellington Street</p>
+              <p>Toronto, Ontario M9C 3J5</p>
+            </div>
+            <div>
+              <strong>Contact</strong>
+              <p>P: +1 253-863-8967</p>
+              <p>M: contact@designo.co</p>
+            </div>
+          </div>
+
+          <div className={`${styles.imageHolder} ${styles.UKMap}`}>
+            <img
+              src="/images/locations/desktop/image-map-uk.png"
+              alt="United Kingdom location"
+            />
+          </div>
+          <div className={`${styles.locationText} ${styles.UKText}`}>
+            <h1>United Kingdom</h1>
+            <div>
+              <strong>Designo Central Office</strong>
+              <p>3886 Wellington Street</p>
+              <p>Toronto, Ontario M9C 3J5</p>
+            </div>
+            <div>
+              <strong>Contact</strong>
+              <p>P: +1 253-863-8967</p>
+              <p>M: contact@designo.co</p>
+            </div>
+          </div>
+        </div>
+      </main>
+      <FooterWithCard />
+    </>
+  );
 };
 
-export default Locations ;
+export default Locations;
