60bfe745809ecccffaa2fa81051f1ac1cf3ad773
codyivy0 <codyivy0@gmail.com>
2024-02-09 14:55:42 -0600

    layout fix

--- src/Pages/Locations.jsx
+++ src/Pages/Locations.jsx
@@ -14,16 +14,18 @@ const Locations = () => {
           </div>
           <div className={`${styles.locationText} ${styles.canadaText}`}>
             <h1>Canada</h1>
-            <div>
-              <strong>Designo Central Office</strong>
-              <p>3886 Wellington Street</p>
-              <p>Toronto, Ontario M9C 3J5</p>
-            </div>
-            <div>
-              <strong>Contact</strong>
-              <p>P: +1 253-863-8967</p>
-              <p>M: contact@designo.co</p>
-            </div>
+            <section className={styles.textGroup}>
+              <div>
+                <strong>Designo Central Office</strong>
+                <p>3886 Wellington Street</p>
+                <p>Toronto, Ontario M9C 3J5</p>
+              </div>
+              <div>
+                <strong>Contact</strong>
+                <p>P: +1 253-863-8967</p>
+                <p>M: contact@designo.co</p>
+              </div>
+            </section>
           </div>
 
           <div className={`${styles.imageHolder} ${styles.australiaMap}`}>
@@ -34,16 +36,18 @@ const Locations = () => {
           </div>
           <div className={`${styles.locationText} ${styles.australiaText}`}>
             <h1>Australia</h1>
-            <div>
-              <strong>Designo Central Office</strong>
-              <p>3886 Wellington Street</p>
-              <p>Toronto, Ontario M9C 3J5</p>
-            </div>
-            <div>
-              <strong>Contact</strong>
-              <p>P: +1 253-863-8967</p>
-              <p>M: contact@designo.co</p>
-            </div>
+            <section className={styles.textGroup}>
+              <div>
+                <strong>Designo AU Office</strong>
+                <p>19 Balonne Street</p>
+                <p>New South Wales 2443</p>
+              </div>
+              <div>
+                <strong>Contact</strong>
+                <p>P: (02) 6720-9092</p>
+                <p>M: contact@designo.au</p>
+              </div>
+            </section>
           </div>
 
           <div className={`${styles.imageHolder} ${styles.UKMap}`}>
@@ -54,16 +58,18 @@ const Locations = () => {
           </div>
           <div className={`${styles.locationText} ${styles.UKText}`}>
             <h1>United Kingdom</h1>
-            <div>
-              <strong>Designo Central Office</strong>
-              <p>3886 Wellington Street</p>
-              <p>Toronto, Ontario M9C 3J5</p>
-            </div>
-            <div>
-              <strong>Contact</strong>
-              <p>P: +1 253-863-8967</p>
-              <p>M: contact@designo.co</p>
-            </div>
+            <section className={styles.textGroup}>
+              <div>
+                <strong>Designo UK Office</strong>
+                <p>13 Colorado Way</p>
+                <p>Rhyd-y-fro SA8 9GA</p>
+              </div>
+              <div>
+                <strong>Contact</strong>
+                <p>P: 078-3115-1400</p>
+                <p>M: contact@designo.uk</p>
+              </div>
+            </section>
           </div>
         </div>
       </main>
