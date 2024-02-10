da29b4141aa402c159c635577fdd30a11fcf8dce
codyivy0 <codyivy0@gmail.com>
2024-02-09 19:56:00 -0600

    started web design, mobile in progress

--- src/Pages/WebDesign.jsx
+++ src/Pages/WebDesign.jsx
@@ -1,10 +1,70 @@
+import FooterWithCard from "../Components/FooterWithCard/FooterWithCard";
+import styles from "./WebDesign.module.css";
 
 const WebDesign = () => {
   return (
-    <div>
-      Web Design
-    </div>
-  )
+    <>
+      <main>
+        <section className={styles.banner}>
+          <h1>Web Design</h1>
+          <p>
+            We build websites that serve as powerful marketing tools and bring
+            memorable brand experiences.
+          </p>
+        </section>
+        <div className={styles.WDWrapper}>
+          <section className={styles.project}>
+            <img src="/images/web-design/image-express.jpg" alt="" />
+            <div className={styles.cardText}>
+              <h1>EXPRESS</h1>
+              <p>A multi-carrier shipping website for exommerce businesses.</p>
+            </div>
+          </section>
+
+          <section className={styles.project}>
+            <img src="/images/web-design/image-express.jpg" alt="" />
+            <div className={styles.cardText}>
+              <h1>EXPRESS</h1>
+              <p>A multi-carrier shipping website for exommerce businesses.</p>
+            </div>
+          </section>
+
+          <section className={styles.project}>
+            <img src="/images/web-design/image-express.jpg" alt="" />
+            <div className={styles.cardText}>
+              <h1>EXPRESS</h1>
+              <p>A multi-carrier shipping website for exommerce businesses.</p>
+            </div>
+          </section>
+
+          <section className={styles.project}>
+            <img src="/images/web-design/image-express.jpg" alt="" />
+            <div className={styles.cardText}>
+              <h1>EXPRESS</h1>
+              <p>A multi-carrier shipping website for exommerce businesses.</p>
+            </div>
+          </section>
+
+          <section className={styles.project}>
+            <img src="/images/web-design/image-express.jpg" alt="" />
+            <div className={styles.cardText}>
+              <h1>EXPRESS</h1>
+              <p>A multi-carrier shipping website for exommerce businesses.</p>
+            </div>
+          </section>
+
+          <section className={styles.project}>
+            <img src="/images/web-design/image-express.jpg" alt="" />
+            <div className={styles.cardText}>
+              <h1>EXPRESS</h1>
+              <p>A multi-carrier shipping website for exommerce businesses.</p>
+            </div>
+          </section>
+        </div>
+      </main>
+      <FooterWithCard />
+    </>
+  );
 };
 
-export default WebDesign ;
+export default WebDesign;
